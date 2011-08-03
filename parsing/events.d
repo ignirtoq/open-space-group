0d7987ad4cd043c8588120ff4f6e544eea342753
xenomorph64 <xenomorph64@gmail.com>
2011-08-03 01:43:54 -0500

    added parsing for ObjectDestructionEvent and FloorTrigger

--- parsing/events.d
+++ parsing/events.d
@@ -19,19 +19,37 @@ private EventParser[string] parsers;
 static this()
 {
 	parsers["HaltSimEvent"] = &parseHaltSimEvent;
+	parsers["ObjectDestructionEvent"] = &parseObjectDestructionEvent;
 }
 
 private Event parseHaltSimEvent(ElementParser xml, SimObject[string] clonableObjects)
 {
-	Trigger eventTrigger;
-	xml.onStartTag["Trigger"] = (ElementParser xml)
-	{
-		assert(eventTrigger is null, "XML has an Event that specifies multiple Triggers");
-		string typeName = xml.tag.attr["type"];
-		eventTrigger = GetTriggerParser(typeName)(xml);
-	};
-	xml.parse();
-
+	Trigger eventTrigger = parseTrigger(xml);
 	assert(eventTrigger !is null, "XML has an Event that does not specify a Trigger");
 	return new HaltSimEvent(eventTrigger);
+}
+
+private Event parseObjectDestructionEvent(ElementParser xml, SimObject[string] clonableObjects)
+{
+	Trigger eventTrigger = parseTrigger(xml);
+	assert(eventTrigger !is null, "XML has an Event that does not specify a Trigger");
+
+	auto newEvent = new ObjectDestructionEvent(eventTrigger);
+	xml.onEndTag["TargetName"] = (in Element e) { newEvent.TargetName = e.text; };
+	xml.parse();
+
+	return newEvent;
+}
+
+private Trigger parseTrigger(ElementParser xml)
+{
+	Trigger trigger;
+	xml.onStartTag["Trigger"] = (ElementParser xml)
+	{
+		assert(trigger is null, "XML has an Event that specifies multiple Triggers");
+		string typeName = xml.tag.attr["type"];
+		trigger = GetTriggerParser(typeName)(xml);
+	};
+	xml.parse();
+	return trigger;
 }
--- parsing/triggers.d
+++ parsing/triggers.d
@@ -8,7 +8,7 @@ public alias Trigger function(ElementParser xml) TriggerParser;
 
 public TriggerParser GetTriggerParser(string typeName)
 {
-	assert(typeName in parsers);
+	assert(typeName in parsers, typeName);
 	return parsers[typeName];
 }
 private TriggerParser[string] parsers;
@@ -16,12 +16,24 @@ private TriggerParser[string] parsers;
 static this()
 {
 	parsers["TurnAroundTrigger"] = &parseTurnAroundTrigger;
+	parsers["FloorTrigger"] = &parseFloorTrigger;
 }
 
 private Trigger parseTurnAroundTrigger(ElementParser xml)
 {
 	auto newTrigger = new TurnAroundTrigger();
 
+	xml.onEndTag["TargetName"] = (in Element e) { newTrigger.TargetName = e.text; };
+	xml.parse();
+
+	return newTrigger;
+}
+
+private Trigger parseFloorTrigger(ElementParser xml)
+{
+	auto newTrigger = new FloorTrigger();
+
+	xml.onEndTag["Floor"] = (in Element e) { newTrigger.Floor = ParseReal(e); };
 	xml.onEndTag["TargetName"] = (in Element e) { newTrigger.TargetName = e.text; };
 	xml.parse();
 
