07c2162817c8fc41f8a32975f28d1e42a4594d07
Ignirtoq <Ignirtoq@gmail.com>
2011-08-03 17:59:11 -0500

    Made idealThrust in Satellite class return zero for actual velocity close enough (2%) to ideal.

--- simObjects.d
+++ simObjects.d
@@ -177,7 +177,7 @@ public class Satellite : SimObject
 	
 	private real idealThrust(Vector3 idealVelocity, Vector3 gravity, real timeStep, EnvironmentService environment)
 	{
-		if ((idealVelocity - Velocity).Length()/idealVelocity.Length() < 0.05)
+		if ((idealVelocity - Velocity).Length() / idealVelocity.Length() < 0.02)
 			return 0.0;
 	
 		return fmin(MaxThrust / Mass, gravity.Length() + (Velocity - idealVelocity).Length() / timeStep);
