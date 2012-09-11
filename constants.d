604506f4e524d61b984e14b22dd99649eb1c1227
Stone <Stone@VikingTower>
2012-09-11 17:25:18 -0500

    + Added ANGULAR_VELOCITY_OF_EARTH to constants.d, which necessitated adding import std.math to that file.  Should be fixed if inappropriate. * Changed DragForce in commonMath.d to reflect a rotating Earth.  This should only affect drag, so that's all that changed. * Updated test.xml with some of the feasibility test simulation parameters.

--- constants.d
+++ constants.d
@@ -1,5 +1,7 @@
 module superAwesome.constants;
 
+import std.math; // Not sure if I should put this somewhere else, but I was getting an error.
+
 /*
 Values in this module are given in the following units
 Mass - Kilograms
@@ -12,5 +14,6 @@ public immutable real GRAVITATIONAL_CONSTANT = 6.67e-11;
 public immutable real MASS_OF_EARTH = 5.97e24;
 public immutable real RADIUS_OF_EARTH = 6371e3;
 public immutable real RADIUS_OF_EARTH_SQUARED = RADIUS_OF_EARTH * RADIUS_OF_EARTH;
+public immutable real ANGULAR_VELOCITY_OF_EARTH = 2 * PI / (24*3600);
 public immutable real AIR_SPECIFIC_GAS_CONSTANT = 287.058;
 public immutable real AIR_SURFACE_TEMPERATURE = 15 + 273.15;
